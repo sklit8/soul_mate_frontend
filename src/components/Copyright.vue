0891876651d4f31ae3bf6d8a953502a3f36e7022
luokunsong <2369228162@qq.com>
2023-06-11 15:29:01 +0800

    主页面完善

--- src/components/Copyright.vue
+++ src/components/Copyright.vue
@@ -1,9 +1,9 @@
 <template>
   <div style="margin-bottom: 30px"></div>
   <div id="copyright">
-    <a href="https://beian.miit.gov.cn/" target="_blank">豫ICP备2023004098号-1</a>
+    <a href="" target="_blank">cdu_SoulMate</a>
     <br>
-    <a target="_blank" href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=41172702000163" style="display:inline-block;text-decoration:none;height:20px;line-height:20px;"><img src="https://img.qimuu.icu/typory/%E5%A4%87%E6%A1%88%E5%9B%BE%E6%A0%87.png" style="float:left;"/><p style="float:left;height:20px;line-height:20px;margin: 0px 0px 0px 5px; color:#939393;">豫公网安备 41172702000163号</p></a>
+    <a target="_blank" href="" style="display:inline-block;text-decoration:none;height:20px;line-height:20px;"><p style="float:left;height:20px;line-height:20px;margin: 0px 0px 0px 5px; color:#939393;">Java期末大项目</p></a>
   </div>
 </template>
 <script>
